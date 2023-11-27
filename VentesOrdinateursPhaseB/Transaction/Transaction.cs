fb5fed4e6368bcea46de0218b62f962f5ee91ae8
Emmanuel Takam <eptakamt01@monccnb.ca>
2023-11-27 12:48:07 -0400

    Ventes Ordinateurs: Phase B

---
+++ VentesOrdinateursPhaseB/Transaction/Transaction.cs
@@ -0,0 +1,142 @@
+﻿/*
+        Programmeurs:   Ange Yemele, 
+                        Ansoumane Condé,
+                        Dorian Wontcheu,
+                        Emmanuel Takam,
+                        Yannis-Arthur Nenzeko
+
+        Date:           Novembre 2023
+
+        Solution:       VentesOrdinateurs.sln
+        Projet:         Transaction.csproj
+        Classe:         Transaction.cs
+
+        But:            Couche métier 
+                        Créer et documenter les membres
+                        Surcharger la méthode GetPrix
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransactionNS
+{
+    /// <summary>
+    /// Objet métier : Classe de transaction
+    /// </summary>
+    /// <remarks>Tous droits réservés: App Creat Inc. 2025</remarks>
+    public class Transaction
+    {
+        #region Déclaration des tableaux
+
+        private string[] tMarqueStr;
+        private string[] tCaracteristiqueStr;
+        private Decimal[,] tPrixDecimal;
+
+        #endregion
+
+        #region Initialisation des tableaux
+
+        public void InitMarques()
+        {
+            tMarqueStr = new string[4] { "Asus", "Dell", "Lenovo", "Msi" };
+        }
+
+        public void InitCaracteristique()
+        {
+            tCaracteristiqueStr = new string[3] { "Core i5", "Core i7", "Core i9" };
+        }
+
+        public void InitPrix()
+        {
+            tPrixDecimal = new decimal[4, 3]
+            {
+                {999.00M,1099.00M,1200.39M },
+                {739.49M,2299.00M,3149.50M },
+                {759.99M,1272.96M,4139.00M },
+                { 769.71M,1299.99M,1899.97M}
+            };
+        }
+
+        #endregion
+
+        #region Constructeur
+
+        public Transaction()
+        {
+            InitMarques();
+            InitCaracteristique();
+            InitPrix();
+        }
+
+        #endregion
+
+        #region Marques de ordinateurs
+
+        public string[] GetMarques()
+        {
+            return tMarqueStr;
+        }
+
+        #endregion
+
+        #region Caractéristique des ordinateurs
+
+        public string[] GetCaracteristiques()
+        {
+            return tCaracteristiqueStr;
+        }
+
+        #endregion
+
+        #region Prix des ordinateurs
+
+        /// <summary>
+        /// Liste des prix
+        /// </summary>
+        /// <param name="indiceMarque">Indice (entier) pour la marque des ordinateurs.</param>
+        /// <param name="indiceCaracteristique">Indice (entier) pour la caractéristique des ordinateurs.</param>
+        /// <returns>Le prix associé à la marque et à la caractéristique d'un ordinateur.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Erreur survenant si un ou les deux indices donnés sont hors des limites des listes des marques et caractéristiques.</exception>
+        public decimal GetPrix(int indiceMarque, int indiceCaracteristique)
+        {
+            if (indiceMarque >= tMarqueStr.GetLowerBound(0) && indiceMarque <= tMarqueStr.GetUpperBound(0))
+            {
+                if (indiceMarque >= tCaracteristiqueStr.GetLowerBound(0) && indiceCaracteristique <= tCaracteristiqueStr.GetUpperBound(0))
+                    return tPrixDecimal[indiceMarque, indiceCaracteristique];
+                else 
+                    throw new ArgumentOutOfRangeException("Le prix n'est pas disponible selon l'indice de la caractéristique reçu.");
+            }
+            else
+                throw new ArgumentOutOfRangeException("Le prix n'est pas disponible selon l'indice de la marque reçu.");            
+        }
+
+        /// <summary>
+        /// Liste des prix
+        /// </summary>
+        /// <param name="marque">Marque (string) d'un ordinateur.</param>
+        /// <param name="caracteristique">Caractéristique (string) d'un ordinateur.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">Erreur survenant si la marque et ou la caractéristique saisies ne sont pas dans les listes des marques et des caractéristiques.</exception>
+        public decimal GetPrix(string marque, string caracteristique)
+        {
+            // Trouver les positions de la marque et de la caractéristique entrées
+            int positionMarque = Array.IndexOf(tMarqueStr, marque);
+            int positionCaracteristique = Array.IndexOf(tCaracteristiqueStr, caracteristique);
+
+            if (positionMarque >= tMarqueStr.GetLowerBound(0) && positionMarque <= tMarqueStr.GetUpperBound(0))
+            {
+                if (positionCaracteristique >= tCaracteristiqueStr.GetLowerBound(0) && positionCaracteristique <= tCaracteristiqueStr.GetUpperBound(0))
+                    return tPrixDecimal[positionMarque, positionCaracteristique];
+                else
+                    throw new ArgumentException("Le prix n'est pas disponible selon la caractéristique reçue.");
+            }
+            else
+                throw new ArgumentException("Le prix n'est pas disponible selon la marque reçue.");
+        }
+
+        #endregion
+    }
+}
